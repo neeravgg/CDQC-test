6b195f031b9afddb49e374598edc7b3fd1086546
neeravgg <neeravgarg663@gmail.com>
2024-03-12 13:45:28 +0530

    feat: initial

---
+++ react-form/src/app/user/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+const Page = () => {
+	const navigate = useRouter();
+
+	return <div>page</div>;
+};
+
+export default Page;
